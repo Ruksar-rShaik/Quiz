8ac33c2b07907f56bdd829b4d3ee86b29c8b6312
ruksar <ruksarrasheeda786@gmail.com>
2023-05-03 18:39:48 +0530

    quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { questions } from "./questions";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [showScore, setShowScore] = React.useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <section className="showScore-section">
+          Your score is {score} out of {questions.length}
+        </section>
+      ) : (
+        <>
+          <section className="question-section">
+            <h1>
+              Question {currentQuestion + 1}/{questions.length}
+            </h1>
+            <p>{questions[currentQuestion].questionText}</p>
+          </section>
+
+          <section className="answer-section">
+            {questions[currentQuestion].answerOptions.map((item) => (
+              <button onClick={() => handleClick(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            ))}
+          </section>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
